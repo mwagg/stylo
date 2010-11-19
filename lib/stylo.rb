38c94741a856a4eede7aa5a14a66edcb37a916e7
mwagg <michael@guerillatactics.co.uk>
2010-11-19 23:34:21 +0000

    fixed railties to default asset_location in rails stopped version.rb being required twice

--- lib/stylo.rb
+++ lib/stylo.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-Dir.glob(File.join(File.dirname(__FILE__), '**/*.rb')).each { |f| require f }
+Dir.glob(File.join(File.dirname(__FILE__), '**/*.rb')).reject do |filename|
+  filename =~ /version.rb$/
+end.each { |f| require f }
 
 
--- lib/stylo/railtie.rb
+++ lib/stylo/railtie.rb
@@ -4,7 +4,7 @@ begin
   module Stylo
     class Railtie < Rails::Railtie
       initializer "stylo.initialize" do |app|
-        Stylo::Config.asset_location ||= File.join(app.root, 'public')
+        Stylo::Config.options[:asset_location] ||= File.join(app.root, 'public')
         app.middleware.insert_before ActionDispatch::Static, Stylo::Rack
       end
     end
